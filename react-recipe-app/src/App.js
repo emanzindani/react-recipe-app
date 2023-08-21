7c4057bfa0bb42d01d76ec2c06ac854fbe550688
Eman <emanamberzindani@gmail.com>
2023-08-21 17:26:36 -0500

    setup query

--- react-recipe-app/src/App.js
+++ react-recipe-app/src/App.js
@@ -9,15 +9,17 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
 
 // empty array in useeffect means that the fetchData function will only be called on initial render and not on subsequent renders when the data set changes
   useEffect(() => {
     getRecipes();
-    }, []);
+    // only going to run when you submit the query
+    }, [query]);
 // add await when fetching any external data
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits)
@@ -27,10 +29,16 @@ const App = () => {
     setSearch(e.target.value)
   }
 
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search)
+    setSearch('')
+  }
+
 
   return (
     <div className= "App">
-      <form className="search-form">
+      <form onSubmit={getSearch}className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
       </form>
